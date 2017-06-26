9b44ee18559a8c18f9fb21671315876f33a808e1
Marcial Garcia Perez <marcial.garcia.perez.contractor@bbva.com>
2017-06-26 20:56:07 +0200

    sys.argv

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# hello-world
-Just another repository
+# hog-detectmultiscale-parameters-explained
+
+Método "clásico" basado en HOG (Histograma de gradientes orientados), que está implementado en las librerías OpenCV. En [este enlace](http://www.pyimagesearch.com/2015/11/16/hog-detectmultiscale-parameters-explained/) explica cómo se puede usar.
+
+## Dataset
+
+[INRIA Person Dataset](http://pascal.inrialpes.fr/data/human/)
+More information can be found on their website.
+
+Images:
+[persons.zip](http://www-old.emt.tugraz.at/~pinz/data/GRAZ_01/)
--- detectmultiscale.py
+++ detectmultiscale.py
@@ -1,9 +1,16 @@
 # import the necessary packages
 from __future__ import print_function
+import sys
 import argparse
 import datetime
 import imutils
 import cv2
+
+print("hola")
+print( cv2.__version__ )
+
+sys.argv = ['detectmultiscale.py', '--image', 'images/person_010.bmp']
+print(sys.argv, len(sys.argv))
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -29,8 +36,18 @@ meanShift = True if args["mean_shift"] > 0 else False
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+print(args["image"])
+
 # load the image and resize it
 image = cv2.imread(args["image"])
+
+#It returns a tuple of number of rows, columns and channels (if image is color):
+print(image.shape)
+numberofcolumns  = image.shape[1]
+print(numberofcolumns)
+print(image.size)
+print(image.dtype)
+
 image = imutils.resize(image, width=min(400, image.shape[1]))
 
 # detect people in the image
@@ -47,3 +64,4 @@ for (x, y, w, h) in rects:
 # show the output image
 cv2.imshow("Detections", image)
 cv2.waitKey(0)
+#cv2.destroyAllWindows()
